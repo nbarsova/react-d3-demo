6cc3de641d1d09a05ef6438b321b0e681ab2c1b2
Natalia Barsova <natfast@gmail.com>
2019-07-24 08:49:52 +0200

    bar chart axes

--- src/BarChart.ts
+++ src/BarChart.ts
@@ -8,11 +8,10 @@ export default class barChart {
     private height: number = 0;
     private width: number = 0;
 
-    private xAxis: any;
-    private yAxis: any;
-
     private yDomain: number[] = [0, 1];
     private yRange: number[] = [0, 100];
+    private xDomain: number[] = [0, 1];
+    private xRange: number[] = [0, 100];
 
     create(rootNode: HTMLDivElement) {
         this.rootNode = rootNode;
@@ -45,33 +44,45 @@ export default class barChart {
             return d[1];
         });
 
-        let minYear = d3.min(this.chartData, (d: any) => {
-            return d[0];
-        });
+        let yearAccessor = (d: any) => {
+            const yearStr: string = d[0];
+            return Number(yearStr.substring(0, yearStr.indexOf("-")));
+        };
 
-        let maxYear = d3.max(this.chartData, (d: any) => {
-            return d[0];
-        });
+        let minYear = d3.min(this.chartData, yearAccessor);
+
+        let maxYear = d3.max(this.chartData, yearAccessor);
 
         let barWidth = this.width / this.chartData.length;
 
         this.yDomain = [0, Number(gdpMax)];
-        this.yRange = [this.height, 0];
+        this.yRange = [this.height-50, 0];
+
+        if (minYear) this.xDomain[0] = minYear;
+        if (maxYear) this.xDomain[1]= maxYear;
+
+        this.xRange=[0, this.width];
 
         let yLineFunction = d3.scaleLinear().domain(this.yDomain).range(this.yRange);
+        let xLineFunction = d3.scaleLinear().domain(this.xDomain).range(this.xRange);
 
         if (this.d3graph) {
 
             let yAxis = d3.axisRight(yLineFunction);
-            let yAxisG = this.d3graph.append("g")
+            this.d3graph.append("g")
                 .call(yAxis);
+
+            let xAxis = d3.axisBottom(xLineFunction);
+            this.d3graph.append("g")
+                .attr("transform", "translate(0, " + (this.height-50) + ")")
+                .call(xAxis);
 
             this.d3graph.selectAll("rect").data(this.chartData)
                 .enter()
                 .append("rect")
                 .attr("width", barWidth)
                 .attr("height", (d: any, i) => {
-                   return this.height - yLineFunction(d[1]);
+                   return this.height -50 - yLineFunction(d[1]);
                 })
                 .attr("x", (d, i) => {
                     return i * barWidth - 2;
